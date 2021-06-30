8160e0ae35bc15c94931516b3c2f52f5923f3498
Rayllanderson <rayllanderson@gmail.com>
2021-06-30 08:29:35 -0300

    Setup inicial

---
+++ src/test/java/br/com/zupacademy/rayllanderson/ecommerce/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.rayllanderson.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
